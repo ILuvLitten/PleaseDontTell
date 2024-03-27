a7d1a9d3ec4b1a5aa8773d782688071c83f15582
ViottiViolet <viottiviolet@gmail.com>
2024-03-27 12:03:18 -0500

    npc data persistence

--- Please Dont Tell/Assets/Scripts/Managers/NPCManager.cs
+++ Please Dont Tell/Assets/Scripts/Managers/NPCManager.cs
@@ -8,7 +8,7 @@ public class NPCManager : MonoBehaviour
     static NPCManager instance;
 
     static bool[] staticHasOrdered = new bool[4];
-    static bool[] staticIsServed = new bool[4];
+    static bool[] staticIsServed = new  bool[4];
     static bool[] staticHasLeft = new bool[4];
 
     void Awake()
@@ -32,13 +32,15 @@ public class NPCManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+    
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(staticHasOrdered[0] + " " + staticHasOrdered[1] + " " + staticHasOrdered[2] + " " + staticHasOrdered[3]);
     }
 
     public IEnumerator StartTimer(NPCController npc)
@@ -51,6 +53,21 @@ public class NPCManager : MonoBehaviour
 
         yield return new WaitForSeconds(30);
         npc.Leave();
+    }
+
+    public void SetHasOrdered(int ID, bool value)
+    {
+        staticHasOrdered[ID] = value;
+    }
+
+    public void SetIsServed(int ID, bool value)
+    {
+        staticIsServed[ID] = value;
+    }
+
+    public void SetHasLeft(int ID, bool value)
+    {
+        staticHasLeft[ID] = value;
     }
 
     public bool GetHasOrdered(int ID)
--- Please Dont Tell/Assets/Scripts/NPCController.cs
+++ Please Dont Tell/Assets/Scripts/NPCController.cs
@@ -11,6 +11,7 @@ public class NPCController : MonoBehaviour
     [SerializeField] bool isCustomer;
     public int ID;
     public int drinkID;
+    public float sceneNum;
 
     [SerializeField] Sprite patience1;
     [SerializeField] Sprite patience2;
@@ -29,11 +30,15 @@ public class NPCController : MonoBehaviour
 
         patienceSprite = transform.GetChild(0).gameObject;
         patienceSprite.GetComponent<SpriteRenderer>().sprite = patience1;
-        patienceSprite.SetActive(false);
-        hasOrdered = NPCManager.GetInstance().GetHasOrdered(ID);
-        isServed = NPCManager.GetInstance().GetIsServed(ID);
-        hasLeft = NPCManager.GetInstance().GetHasLeft(ID);
-        if (hasLeft) Destroy(gameObject);
+        
+        if (GameStateManager.GetInstance().GetDayBool(sceneNum))
+        {
+            StartDay();
+        }
+        else
+        {
+            NotStartDay();
+        }
         
     }
 
@@ -45,21 +50,23 @@ public class NPCController : MonoBehaviour
 
     public void InitiateDialogue(int[] inventory)
     {
-        if (isCustomer && !isServed)
+        if (isCustomer)
         {
-            if (!patienceSprite.activeSelf)
+            if (!hasOrdered)
             {
                 DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
                 patienceSprite.SetActive(true);
+                hasOrdered = true;
+                NPCManager.GetInstance().SetHasOrdered(ID, true);
             }
-            else if (inventory[drinkID] > 0) 
+            else if (inventory[drinkID] > 0 && !isServed) 
             {
                 DialogueManager.GetInstance().EnterDialogueMode(inkJSONserved);
                 patienceSprite.SetActive(false);
                 inventory[drinkID] -= 1;
                 LiquorCount.GetInstance().UpdateCount(inventory);
                 isServed = true;
-                //inventory[itemID] -= 1;
+                NPCManager.GetInstance().SetIsServed(ID, true);
             }
         }
         else if (!isCustomer) DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
@@ -71,7 +78,7 @@ public class NPCController : MonoBehaviour
         Debug.Log("patron " + ID + " has left");
     }
 
-    public void SetPatienceSprite(int patience)
+    /*public void SetPatienceSprite(int patience)
     {
 
         switch(patience)
@@ -81,5 +88,21 @@ public class NPCController : MonoBehaviour
                 break;
         }
 
+    }*/
+
+    void StartDay()
+    {
+        patienceSprite.SetActive(false);
+        NPCManager.GetInstance().SetHasOrdered(ID, false);
+        NPCManager.GetInstance().SetIsServed(ID, false);
+        NPCManager.GetInstance().SetHasLeft(ID, false);
+    }
+
+    void NotStartDay()
+    {
+        hasOrdered = NPCManager.GetInstance().GetHasOrdered(ID);
+        isServed = NPCManager.GetInstance().GetIsServed(ID);
+        hasLeft = NPCManager.GetInstance().GetHasLeft(ID);
+        patienceSprite.SetActive(hasOrdered);
     }
 }
